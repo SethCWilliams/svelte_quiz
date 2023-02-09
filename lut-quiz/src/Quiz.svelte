550923bc588dfc5bb7e6ae336d9064aad831f822
sethwilliams <fullstackseth@gmail.com>
2023-02-08 23:34:42 -0500

    first commit

---
+++ lut-quiz/src/Quiz.svelte
@@ -0,0 +1,25 @@
+<script>
+  let result = "";
+  let correctAnswer = "b";
+  let answers = ["a", "b", "c", "d"];
+
+  function pickAnswer(answer) {
+    if (answer === correctAnswer) {
+      return (result = "Correct!");
+    }
+    result = "OOPS!";
+  }
+</script>
+
+<div>
+  {#if result}
+    <h4>{result}</h4>
+  {:else}
+    <h4>Please pick an answer</h4>
+  {/if}
+  {#each answers as answer}
+    <button on:click={() => pickAnswer(answer)}>
+      Answer {answer.toUpperCase()}
+    </button>
+  {/each}
+</div>
