4499e247e4edfff49dd474d6329ee19092b32b78
sethwilliams <fullstackseth@gmail.com>
2023-02-10 16:54:00 -0500

    add modal

---
+++ lut-quiz/src/Modal.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { fade, blur, fly, slide, scale } from "svelte/transition";
+</script>
+
+<div transition:fade={{ x: 100 }} class="modal-bg">
+  <div transition:fly={{ y: 100 }} class="modal">
+    <button> Close </button>
+    <slot />
+    <!-- <slot>optional fallback</slot> -->
+  </div>
+</div>
+
+<style>
+  .modal-bg {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: rgba(0, 0, 0, 0.8);
+  }
+  .modal {
+    background: white;
+    padding: 20px;
+    border-radius: 15px;
+  }
+</style>
